1b3f70b709e27a2d44d2ab8d70fb9b9bc3c311ac
Taylor Hartley <twh0008@auburn.edu>
2018-04-27 18:37:21 -0500

    Add css, Add html, Add js file

--- app.js
+++ app.js
@@ -4,17 +4,14 @@ const   express     = require("express"),
         port        = process.env.PORT || 3000;
 
 
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
-
+app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/views`));
 const todoRoutes = require(`./routes/todos`)
 
 app.get("/", (req, res) => {
-    res.send(`hello from root!`);
-
+    res.sendFile("index.html");
 });
 
 app.use(`/api/todos`, todoRoutes);

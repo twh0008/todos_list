6a612e3bc93fb01f8d911588fa98b64f6d98e5f7
Taylor Hartley <twh0008@auburn.edu>
2018-04-27 22:06:36 -0500

    Add functionality for todo list

--- public/index.js
+++ public/index.js
@@ -6,12 +6,34 @@ $(document).ready(() => {
         console.log(err);
     });
 
+    $('#todoInput').keypress(event => {
+      if (event.which === 13) {
+        createTodo();
+      }
+    })
+
 
 });
 function addTodos(todos) {
   todos.forEach(todo => {
-    let newTodo = $('<li>' + todo.name + '</li>');
-    $(".list").append(newTodo);
-    console.log(todo.name);
+    addTodo(todo);
   });
+}
+function createTodo() {
+  let userInput = $('#todoInput').val();
+  let createdTodo = {name: userInput};
+  $.post('api/todos', createdTodo)
+  .then(newTodo => {
+    addTodo(newTodo);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+}
+
+function addTodo(todo){
+    let newTodo = $("<li>" + todo.name + "</li>");
+    if (todo.completed) {newTodo.addClass("done");}
+    newTodo.addClass("task");
+    $(".list").append(newTodo);
 }
